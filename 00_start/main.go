1a4b8643311f7f067629e0d5946a96cd610a7c3f
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-03-08 13:06:09 +0100

    go

--- 00_start/main.go
+++ 00_start/main.go
@@ -4,5 +4,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Gogogogogo, it's a go!")
+	fmt.Println("Said van de Klundert trying on some go.")
+	fmt.Println("Gogogogogo, it's a go!", "gogogo")
+	fmt.Print("Gogogogogo, it's a go!")
 }
