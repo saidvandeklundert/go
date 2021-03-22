3978839e1f5e96d575942629439a29dc45025ae8
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-03-22 09:31:51 +0100

    go

--- examples/functions.go
+++ examples/functions.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
+func main() {
+	fmt.Println("Some example functions.")
+	var str_value string = "word"
+	echo(str_value)
+	var int_value int = 100
+	new_int := multiply(int_value)
+	// Explaining what multiply returned:
+	fmt.Printf(`
+int_value now:                 %v
+int_ptr:                       %v
+new_int:                       %v
+new_int pointer value:		   %v
+
+`, int_value, &int_value, new_int, &new_int)
+	// Old int left as is
+	// Change int_value:
+	multiplyExisting(&int_value)
+	fmt.Printf(`
+int_value now:                 %v
+int_ptr:                       %v
+
+`, int_value, &int_value)
+
+	// Illustrate we can pass a func + arg as arg to a func:
+	x_1 := returnInt(silent_multiply(int_value))
+	fmt.Printf("\nreturnInt(silent_multiply(int_value)) got us: %d\n", x_1)
+
+	// Illustrate naked return:
+	_ = echoReturnString(str_value)
+
+}
+
 // Echo a string:
 func echo(s string) {
 	fmt.Printf("%s\n", s)
@@ -30,33 +62,11 @@ func silent_multiply(i int) int {
 	return i
 }
 
-// Make a copy o
-
-func main() {
-	fmt.Println("Some example functions.")
-	var str_value string = "word"
-	echo(str_value)
-	var int_value int = 100
-	new_int := multiply(int_value)
-	// Explaining what multiply returned:
-	fmt.Printf(`
-int_value now:                 %v
-int_ptr:                       %v
-new_int:                       %v
-new_int pointer value:		   %v
-
-`, int_value, &int_value, new_int, &new_int)
-	// Old int left as is
-	// Change int_value:
-	multiplyExisting(&int_value)
-	fmt.Printf(`
-int_value now:                 %v
-int_ptr:                       %v
-
-`, int_value, &int_value)
-
-	// Illustrate we can pass a func + arg as arg to a func:
-	x_1 := returnInt(silent_multiply(int_value))
-	fmt.Printf("\nreturnInt(silent_multiply(int_value)) got us: %d\n", x_1)
+// Naked return:
+func echoReturnString(s string) (str string) {
+	str = s
+	fmt.Printf("%s\n", str)
+	return
+	// Due to (str string) this is the same as return str
 
 }
